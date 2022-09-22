08a4d0e2dbbd2a469330bfb9aa335a3f2c3a069f
Tomás Gonçalves <tomasricardo1975@outlook.pt>
2022-09-22 19:40:26 +0100

    Fix React

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
+
+window.React = React
+
 function App() {
   return (
     <>
